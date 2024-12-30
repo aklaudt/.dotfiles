39566c6402d59aa73525661acf784fd1f3173d85
aklaudt <andrewklaudt@gmail.com>
2024-12-30 09:35:10 -0600

    Update sessionizer script
    Remove duplicate sessionizer script
    Update script to use --tmux flag
    Filter out extra parts of file path

--- scripts/.config/bin/tmux-sessionizer.sh
+++ scripts/.config/bin/tmux-sessionizer.sh
@@ -1,37 +1,38 @@
 #!/usr/bin/env bash
 
-COMMON_PREFIX="/home/andrewklaudt/../../mnt/c"
-
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find "$COMMON_PREFIX/personal" "$COMMON_PREFIX/repos" -mindepth 1 -maxdepth 1 -type d | sed "s|^$COMMON_PREFIX/||" | sort | fzf)
+    selected=$(find ~/../../mnt/c/repos/ ~/../../mnt/c/personal/ -mindepth 1 -maxdepth 1 -type d | sed -e 's/\/home\/andrewklaudt\/..\/..\/mnt\/c\///' | fzf --height 70% --tmux 70%)
 fi
 
 if [[ -z $selected ]]; then
+    echo "No selection made. Exiting."
     exit 0
 fi
 
-# Add the common prefix back to the selected path
-selected="$COMMON_PREFIX/$selected"
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
-# Check if tmux is running and create a new session if necessary
+# Start a new tmux session if tmux is not running
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    tmux new-session -d -s $selected_name -c "$selected" -n "nvim" "nvim ."
-    tmux new-window -t "$selected_name" -n "other"
-    tmux switch-client -t "$selected_name"
+    tmux new-session -s "$selected_name" -c "$selected" -d
+    tmux send-keys -t "$selected_name" "nvim" C-m
+    tmux split-window -t "$selected_name" -c "$selected"
+    tmux attach-session -t "$selected_name"
     exit 0
 fi
 
-# Create a new session if it doesn't already exist
+# Create a new tmux session if it doesn't exist
 if ! tmux has-session -t="$selected_name" 2> /dev/null; then
-    tmux new-session -d -s "$selected_name" -c "$selected" -n "nvim" "nvim ."
-    tmux new-window -t "$selected_name" -n "other"
-    tmux switch-client -t "$selected_name"
+    tmux new-session -s "$selected_name" -c "$selected" -d
+    tmux send-keys -t "$selected_name" "nvim" C-m
+    tmux split-window -t "$selected_name" -c "$selected"
 fi
 
-tmux switch-client -t "$selected_name"
-tmux send-keys -t "$selected_name" "cd $selected" C-m
-tmux select-window -t "$selected_name:nvim"
+# Switch to the tmux session
+if [[ -n $TMUX ]]; then
+    tmux switch-client -t "$selected_name"
+else
+    tmux attach-session -t "$selected_name"
+fi
