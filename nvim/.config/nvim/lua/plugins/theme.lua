ccc0b1084caf84bcf180f16bedf4cb99e14fb5b4
Andrew Klaudt <andrewklaudt@gmail.com>
2025-10-29 09:47:55 -0500

    Update theme

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,41 +1,12 @@
-  -- return {
-  --   -- Theme inspired by Atom
-  --   'navarasu/onedark.nvim',
-  --   priority = 1000,
-  --   config = function()
-  --     vim.cmd.colorscheme 'onedark'
-  --   end,
-  -- }
-
-  -- return {
-  --   'rebelot/kanagawa.nvim',
-  --     config = function()
-  --     vim.cmd.colorscheme 'kanagawa-lotus'
-  --   end,
-  -- }
-  --
---   return {
---   'sainnhe/everforest',
---   lazy = false,
---   priority = 1000,
---   config = function()
---     -- Optionally configure and load the colorscheme
---     -- directly inside the plugin declaration.
---     vim.g.everforest_enable_italic = true
---     vim.cmd.colorscheme('everforest')
---   end
--- }
- -- lua/plugins/rose-pine.lua
--- return {
---   "folke/tokyonight.nvim",
---   lazy = false,
---   priority = 1000,
---   opts = {},
--- }
-return { 
-	"rose-pine/neovim", 
-	name = "rose-pine",
-	config = function()
-		vim.cmd("colorscheme rose-pine-moon")
-	end
+return {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            flavour = "frappe",
+            auto_integrations = true,
+        })
+        vim.cmd.colorscheme("catppuccin")
+    end,
 }
